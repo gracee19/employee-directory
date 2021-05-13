de7d95d86109eb3084e6326e2c9a8c8ed7b724ec
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-05-13 18:24:15 +0930

    updated folder

--- src/components/Searchform/index.js
+++ src/components/Searchform/index.js
@@ -3,41 +3,46 @@ import "./style.css";
 
 function Searchform(props) {
   return (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="users">Please Type Username</label>
-        <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="user"
-          list="users"
-          type="text"
-          className="form-control"
-          placeholder="Type the username here"
-          id="employees"
-        />
-
-        <table id="users">
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Age</th>
-            <th>Gender</th>
+    <div>
+      <form className="search">
+        <div className="form-group">
+          <label htmlFor="users">Please Type Username</label>
+          <input
+            value={props.search}
+            onChange={props.handleInputChange}
+            name="user"
+            list="users"
+            type="text"
+            className="form-control"
+            placeholder="Type the username here"
+            id="employees"
+          />
+        </div>
+      </form>
+      <table id="users">
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        {props.users.map((user, index) => (
+          <tr key={index}>
+            {console.log(user)}
+            <td>
+              <img src={user.picture.medium}></img>
+            </td>
+            <td>
+              {user.name.title + " " + user.name.first + " " + user.name.last}
+            </td>
+            <td>{user.email}</td>
+            <td>{user.dob.age}</td>
+            <td>{user.gender}</td>
           </tr>
-          {props.users.map((user, index) => (
-            <tr key={index}>
-              {console.log(user)}
-              <td><img src={user.picture.medium}></img></td>
-              <td>{user.name.title + " " + user.name.first + " " + user.name.last}</td>
-              <td>{user.email}</td>
-              <td>{user.dob.age}</td>
-              <td>{user.gender}</td>
-            </tr>
-          ))}
-        </table>
-      </div>
-    </form>
+        ))}
+      </table>
+    </div>
   );
 }
 
